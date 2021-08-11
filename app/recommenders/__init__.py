48f04239c87b5e82ce1e8ea3c417c67c276e0a01
rachitest <rachit-sabharwal@outlook.com>
2021-08-11 23:04:08 +0000

    update recommenders __init__

--- app/recommenders/__init__.py
+++ app/recommenders/__init__.py
@@ -1,2 +1,3 @@
 from .bm25_rec import createBMObject, getBM25Ranks, getRecs
 from .doc2vec_rec import createDoc2VecObject, createModel, getDoc2VecScores, getDoc2VecRecs
+from .tfidf_rec import createVectorizer, createTFIDFModel, processQuery, getTFIDFRecs
